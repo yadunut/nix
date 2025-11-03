d6fd2eca4cd525f3b7ce74a097c2a7191d8356fc
Yadunand Prem <yadunand@yadunut.com>
2025-11-03 17:16:01 +0800

    homebrew with nix-darwin

--- machines/yadunut-mbp/configuration.nix
+++ machines/yadunut-mbp/configuration.nix
@@ -13,10 +13,64 @@ in
   users.users."root" = {
     openssh.authorizedKeys.keys = [ keys.yadunut ];
   };
+
+  homebrew = {
+    enable = true;
+    onActivation.cleanup = "zap";
+    greedyCasks = true;
+    casks = [
+      "1password"
+      "1password-cli"
+      "calibre"
+      "cardhop"
+      "coconutbattery"
+      "daisydisk"
+      "darktable"
+      "datagrip"
+      "discord"
+      "fantastical"
+      "ghostty"
+      "google-chrome"
+      "iina"
+      "keybase"
+      "kicad"
+      "launchcontrol"
+      "ledger-live"
+      "lens"
+      "logitech-g-hub"
+      "logseq"
+      "loom"
+      "lulu"
+      "obs"
+      "obsidian"
+      "protonvpn"
+      "raycast"
+      "skim"
+      "slack"
+      "spotify"
+      "steam"
+      "superwhisper"
+      "syncthing-app"
+      "tailscale"
+      "tailscale-app"
+      "telegram"
+      "the-unarchiver"
+      "transmission"
+      "visual-studio-code"
+      "whatsapp"
+      "xcodes-app"
+      "yaak"
+      "zen"
+      "zerotier-one"
+      "zoom"
+      "zotero"
     ];
   };
 
+  fonts.packages = [ pkgs.jetbrains-mono ];
+
   security.pam.services.sudo_local.touchIdAuth = true;
+
   nixpkgs.hostPlatform = "aarch64-darwin";
   clan.core.networking.targetHost = "root@localhost";
   nix.enable = false; # since we're using nix darwin
