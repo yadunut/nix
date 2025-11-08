399f261b15fafc0bf405c8688de18f1210b3432e
Yadunand Prem <yadunand@yadunut.com>
2025-11-09 00:47:49 +0800

    mbp updates

--- machines/yadunut-mbp/configuration.nix
+++ machines/yadunut-mbp/configuration.nix
@@ -1,95 +1,95 @@
-{
-  pkgs,
-  inputs,
-  ...
-}:
+{ pkgs, inputs, ... }:
+let
+  inherit (import ../../lib) collectNixFiles;
+in
 {
   imports = [
     inputs.home-manager.darwinModules.home-manager
     inputs.nix-homebrew.darwinModules.nix-homebrew
-    ../../modules/common/sane-defaults
-    ../../modules/common/users
-  ];
-  system.primaryUser = "yadunut";
+  ]
+  ++ collectNixFiles ../../modules/common;
+  # ++ collectNixFiles ../../modules/darwin;
+  config = {
+    system.primaryUser = "yadunut";
 
-  nut = {
-    users.enable = true;
-    sane-defaults.enable = true;
+    nut = {
+      users.enable = true;
+      sane-defaults.enable = true;
+    };
+
+    nix-homebrew = {
+      enable = true;
+      user = "yadunut";
+      autoMigrate = true;
+    };
+
+    # # Home Manager configuration
+    home-manager.useUserPackages = true;
+    home-manager.users.yadunut = {
+      imports = [
+        ./homes/yadunut.nix
+        inputs.nixvim.homeModules.nixvim
+      ];
+    };
+
+    nixpkgs.config.allowUnfree = true;
+
+    fonts.packages = [ pkgs.jetbrains-mono ];
+    homebrew = {
+      enable = true;
+      onActivation.cleanup = "zap";
+      greedyCasks = true;
+      casks = [
+        "1password"
+        "1password-cli"
+        "calibre"
+        "coconutbattery"
+        "daisydisk"
+        "darktable"
+        "datagrip"
+        "discord"
+        "fantastical"
+        "fastmail"
+        "ghostty"
+        "google-chrome"
+        "iina"
+        "keybase"
+        "kicad"
+        "launchcontrol"
+        "ledger-live"
+        "lens"
+        "logitech-g-hub"
+        "logseq"
+        "loom"
+        "lulu"
+        "obs"
+        "obsidian"
+        "protonvpn"
+        "raycast"
+        "skim"
+        "slack"
+        "spotify"
+        "steam"
+        "syncthing-app"
+        "tailscale"
+        "tailscale-app"
+        "telegram"
+        "the-unarchiver"
+        "transmission"
+        "visual-studio-code"
+        "whatsapp"
+        "xcodes-app"
+        "yaak"
+        "zen"
+        "zerotier-one"
+        "zoom"
+        "zotero"
+      ];
+    };
+
+    nixpkgs.hostPlatform = "aarch64-darwin";
+    clan.core.networking.targetHost = "root@localhost";
+    nix.enable = false; # since we're using nix darwin and determinate nix
+    system.stateVersion = 6;
   };
-
-  nix-homebrew = {
-    enable = true;
-    user = "yadunut";
-    autoMigrate = true;
-  };
-
-  # # Home Manager configuration
-  home-manager.useUserPackages = true;
-  home-manager.users.yadunut = {
-    imports = [
-      ./homes/yadunut.nix
-      inputs.nixvim.homeModules.nixvim
-    ];
-  };
-
-  nixpkgs.config.allowUnfree = true;
-
-  homebrew = {
-    enable = true;
-    onActivation.cleanup = "zap";
-    greedyCasks = true;
-    casks = [
-      "1password"
-      "1password-cli"
-      "calibre"
-      "coconutbattery"
-      "daisydisk"
-      "darktable"
-      "datagrip"
-      "discord"
-      "fantastical"
-      "fastmail"
-      "ghostty"
-      "google-chrome"
-      "iina"
-      "keybase"
-      "kicad"
-      "launchcontrol"
-      "ledger-live"
-      "lens"
-      "logitech-g-hub"
-      "logseq"
-      "loom"
-      "lulu"
-      "obs"
-      "obsidian"
-      "protonvpn"
-      "raycast"
-      "skim"
-      "slack"
-      "spotify"
-      "steam"
-      "syncthing-app"
-      "tailscale"
-      "tailscale-app"
-      "telegram"
-      "the-unarchiver"
-      "transmission"
-      "visual-studio-code"
-      "whatsapp"
-      "xcodes-app"
-      "yaak"
-      "zen"
-      "zerotier-one"
-      "zoom"
-      "zotero"
-    ];
-  };
-
-  fonts.packages = [ pkgs.jetbrains-mono ];
-
-  nixpkgs.hostPlatform = "aarch64-darwin";
-  clan.core.networking.targetHost = "root@localhost";
-  nix.enable = false; # since we're using nix darwin and determinate nix
-  system.stateVersion = 6;
 }
