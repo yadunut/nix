b7d375d94841d67cd283d927d7e56642a3fa3378
Yadunand Prem <yadunand@yadunut.com>
2025-11-28 00:47:48 +0800

    premhome-falcon-1

---
+++ modules/configurations/premhome-falcon-1/default.nix
@@ -0,0 +1,51 @@
+{
+  config,
+  ...
+}:
+let
+  hostname = "premhome-falcon-1";
+  hosts = import ../../../hosts.nix;
+  ip = hosts.machines.${hostname}.ip;
+  serverIp = hosts.machines.nut-gc1.ip;
+  nixosModules = config.flake.modules.nixos;
+  homeManagerModules = config.flake.modules.homeManager;
+in
+{
+  configurations.nixos.${hostname}.module =
+    { config, ... }:
+    {
+      imports = with nixosModules; [
+        base
+        home-manager
+        k3s
+        tailscale
+        yadunut
+        zerotier
+      ];
+      home-manager.users.yadunut.imports = with homeManagerModules; [
+        nixvim
+        base
+        yadunut
+      ];
+      age.secrets.k3s.file = ../../secrets/k3s.age;
+      nut = {
+        boot.loader = "systemd";
+        k3s = {
+          enable = true;
+          role = "agent";
+          tokenFile = config.age.secrets.k3s.path;
+          serverAddr = "https://${serverIp}:6443";
+          nodeIp = ip;
+          iface = "ztxh6lvd6t";
+        };
+      };
+
+      networking.hostName = hostname;
+
+      services = {
+        qemuGuest.enable = true;
+      };
+
+      system.stateVersion = "24.11";
+    };
+}
---
+++ modules/configurations/premhome-falcon-1/hardware.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  inputs,
+  ...
+}:
+{
+  configurations.nixos.premhome-falcon-1.module = {
+    imports = [ inputs.nixpkgs.nixosModules.notDetected ];
+
+    boot.initrd.availableKernelModules = [
+      "uhci_hcd"
+      "ehci_pci"
+      "ahci"
+      "virtio_pci"
+      "virtio_scsi"
+      "sd_mod"
+      "sr_mod"
+    ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = [ ];
+
+    networking.useDHCP = lib.mkDefault true;
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  };
+}
