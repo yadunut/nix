cf11d732f4604fdaaadb3196f1eb61f8d9805f01
Yadunand Prem <yadunand@yadunut.com>
2025-11-28 15:19:59 +0800

    fix penguin's nvidia

--- modules/configurations/penguin/hardware.nix
+++ modules/configurations/penguin/hardware.nix
@@ -1,25 +1,26 @@
 {
   inputs,
-  config,
   lib,
   ...
 }:
 {
-  configurations.nixos.penguin.module = {
-    imports = [ inputs.nixpkgs.nixosModules.notDetected ];
+  configurations.nixos.penguin.module =
+    { config, ... }:
+    {
+      imports = [ inputs.nixpkgs.nixosModules.notDetected ];
 
-    boot.initrd.availableKernelModules = [
-      "xhci_pci"
-      "nvme"
-      "usbhid"
-      "usb_storage"
-      "sd_mod"
-    ];
-    boot.initrd.kernelModules = [ ];
-    boot.kernelModules = [ "kvm-amd" ];
-    boot.extraModulePackages = [ ];
-    networking.useDHCP = lib.mkDefault true;
-    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  };
+      boot.initrd.availableKernelModules = [
+        "xhci_pci"
+        "nvme"
+        "usbhid"
+        "usb_storage"
+        "sd_mod"
+      ];
+      boot.initrd.kernelModules = [ ];
+      boot.kernelModules = [ "kvm-amd" ];
+      boot.extraModulePackages = [ ];
+      networking.useDHCP = lib.mkDefault true;
+      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+      hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    };
 }
--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -8,6 +8,6 @@
     };
     hardware.graphics.enable = true;
     services.xserver.videoDrivers = [ "nvidia" ];
-    hardware.nvidia.open = true;
+    hardware.nvidia.open = false;
   };
 }
